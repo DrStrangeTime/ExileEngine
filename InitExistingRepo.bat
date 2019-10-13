4bc58e00df5f2a2e90a871cb83fabab20f381b07
DrStrangeTime <williamtwheate@hotmail.com>
2019-10-13 23:57:14 +0100

    New Update

--- InitExistingRepo.bat
+++ InitExistingRepo.bat
@@ -1,6 +1,9 @@
+@ echo $PWD
 git init
 git status
 git config --global user.email williamtwheate@hotmail.com
 git config --global user.name DrStrangeTime
 git status
+git remote add origin https://github.com/DrStrangeTime/ExileEngine.git
+git remote -v
 PAUSE
--- PullOnNewDevice.bat
+++
@@ -1,4 +0,0 @@
-git fetch --all
-git reset --hard origin/masters
-git status
-PAUSE
