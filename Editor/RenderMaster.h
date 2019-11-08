c232d15544637804a74ce493cba4f842d806aead
Williams3DWorld <williamtwheate@hotmail.com>
2019-11-08 20:35:58 +0000

    New Update

--- Editor/RenderMaster.h
+++ Editor/RenderMaster.h
@@ -1,10 +1,18 @@
 #ifndef __RENDER_MASTER_H__
 #define __RENDER_MASTER_H__
 
+#define API_OPENGL	0x0
+#define API_VULKAN	0x1
+
 #include "GLRenderPipeline.h"
 
+/* This class will filter all of render data using a specific render mode and render API */
 class RenderMaster
 {
+private:
+	static uint32_t _api;
+	static uint32_t _mode;
+
 public:
 
 };
